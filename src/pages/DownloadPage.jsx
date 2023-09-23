bc65ec5467fcea18c942dafa27165dd073870f25
Abdulmas01 <auwalabdul2015@gmail.com>
2023-09-23 14:40:13 +0100

    added app download link

--- src/pages/DownloadPage.jsx
+++ src/pages/DownloadPage.jsx
@@ -10,19 +10,9 @@ const DownloadPage = () => {
   // }, [params.app])
   const downloadLinks = [
     {
-      androidVersion: "Android 10 and Above",
-      downloadLink: "http://h.com",
-      fileSize: "22mb"
-    },
-    {
-      androidVersion: "Android 8 and Lower",
-      downloadLink: "",
-      fileSize: "19mb"
-    },
-    {
-      androidVersion: "Any Version",
-      downloadLink: "",
-      fileSize: "52mb"
+      androidVersion: "Searchit",
+      downloadLink: "https://drive.google.com/file/d/1DTrctWDw-rMdfVOmAK0clp72v5YyC86P/view?usp=drive_link",
+      fileSize: "35mb"
     },
 
   ]
@@ -30,23 +20,10 @@ const DownloadPage = () => {
   return (
     <>
       <h1>Android</h1>
+      <p>click to download</p>
       {downloadLinks.map((value, index) => {
         return <DownloadContainer key={index} dowloadlink={value.downloadLink} name={value.androidVersion} image={searchit} filesize={value.fileSize} />
       })}
-      <section className='english buitify'>
-        <h1>How to Checking Android version</h1>
-        <p>1. open setting in your android phone</p>
-        <p>2.click on search</p>
-        <p>3.search Android version</p>
-        <p>4.in the tile you will see your android version</p>
-      </section>
-      <section className='hausa buitify'>
-        <h1>Yadda Ake dubba Android Version</h1>
-        <p>1. Ka bude settings Akan android phone naka</p>
-        <p>2.kadu duba icon na search(na bincike)</p>
-        <p>3.kayi bin cika Android Version</p>
-        <p>4.a tile din zakaga android version nake</p>
-      </section>
     </>
 
   )
